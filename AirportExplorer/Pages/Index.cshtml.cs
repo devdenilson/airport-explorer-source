aa80c1ad55fe4967fbf27cb7ef0048dd015c5590
Jerrie Pelser <jerrie@jerriepelser.com>
2017-11-23 11:39:13 +0700

    Add map

--- AirportExplorer/Pages/Index.cshtml.cs
+++ AirportExplorer/Pages/Index.cshtml.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AirportExplorer.Pages
 {
     public class IndexModel : PageModel
     {
+        public string MapboxAccessToken { get; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            MapboxAccessToken = configuration["Mapbox:AccessToken"];
+        }
+
         public void OnGet()
         {
 
