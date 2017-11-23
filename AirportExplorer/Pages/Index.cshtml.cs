d855294df0dc9509336203be4cfc1cea7f235623
Jerrie Pelser <jerrie@jerriepelser.com>
2017-11-23 15:37:58 +0700

    Display airports

--- AirportExplorer/Pages/Index.cshtml.cs
+++ AirportExplorer/Pages/Index.cshtml.cs
@@ -1,20 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 
 namespace AirportExplorer.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
         public string MapboxAccessToken { get; }
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
+
             MapboxAccessToken = configuration["Mapbox:AccessToken"];
         }
 
         public void OnGet()
         {
+        }
 
+        public IActionResult OnGetAirports()
+        {
+            var configuration = new Configuration
+            {
+                BadDataFound = context => { }
+            };
+
+            using (var sr = new StreamReader(Path.Combine(_hostingEnvironment.WebRootPath, "airports.dat")))
+            using (var reader = new CsvReader(sr, configuration))
+            {
+                FeatureCollection featureCollection = new FeatureCollection();
+
+                while (reader.Read())
+                {
+                    string name = reader.GetField<string>(1);
+                    string iataCode = reader.GetField<string>(4);
+                    double latitude = reader.GetField<double>(6);
+                    double longitude = reader.GetField<double>(7);
+
+                    featureCollection.Features.Add(new Feature(
+                        new Point(new Position(latitude, longitude)),
+                        new {Name = name, IataCode = iataCode}));
+                }
+
+                return new JsonResult(featureCollection);
+            }
         }
     }
 }
