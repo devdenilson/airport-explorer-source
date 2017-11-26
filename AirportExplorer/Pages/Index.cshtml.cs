177f6a51db67987cb2b34c9969f706207c8b2aa3
Jerrie Pelser <jerrie@jerriepelser.com>
2017-11-26 08:37:05 +0700

    Add airport detail handler

--- AirportExplorer/Pages/Index.cshtml.cs
+++ AirportExplorer/Pages/Index.cshtml.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using CsvHelper;
 using CsvHelper.Configuration;
 using GeoJSON.Net.Feature;
 using GeoJSON.Net.Geometry;
+using GoogleApi;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Places.Details.Request;
+using GoogleApi.Entities.Places.Photos.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace AirportExplorer.Pages
 {
@@ -16,12 +27,14 @@ namespace AirportExplorer.Pages
     {
         private readonly IHostingEnvironment _hostingEnvironment;
         public string MapboxAccessToken { get; }
+        public string GoogleApiKey { get; }
 
         public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
 
             MapboxAccessToken = configuration["Mapbox:AccessToken"];
+            GoogleApiKey = configuration["google:ApiKey"];
         }
 
         public void OnGet()
@@ -58,6 +71,66 @@ namespace AirportExplorer.Pages
 
                 return new JsonResult(featureCollection);
             }
+        }
+
+        public async Task<IActionResult> OnGetAirportDetail(string name, double latitude, double longitude)
+        {
+            var airportDetail = new AirportDetail();
+
+            // Execute the search request
+            var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
+            {
+                Key = GoogleApiKey,
+                Name = name,
+                Location = new Location(latitude, longitude),
+                Radius = 1000
+            });
+
+            // If we did not get a good response, or the list of results are empty then get out of here
+            if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
+                return new BadRequestResult();
+
+            // Get the first result
+            var nearbyResult = searchResponse.Results.FirstOrDefault();
+            string placeId = nearbyResult.PlaceId;
+            string photoReference = nearbyResult.Photos.FirstOrDefault()?.PhotoReference;
+            string photoCredit = nearbyResult.Photos.FirstOrDefault()?.HtmlAttributions.FirstOrDefault();
+
+            // Execute the details request
+            var detailsResonse = await GooglePlaces.Details.QueryAsync(new PlacesDetailsRequest
+            {
+                Key = GoogleApiKey,
+                PlaceId = placeId
+            });
+
+            // If we did not get a good response then get out of here
+            if (!detailsResonse.Status.HasValue || detailsResonse.Status.Value != Status.Ok)
+                return new BadRequestResult();
+
+            // Set the details
+            var detailsResult = detailsResonse.Result;
+            airportDetail.FormattedAddress = detailsResult.FormattedAddress;
+            airportDetail.PhoneNumber = detailsResult.InternationalPhoneNumber;
+            airportDetail.Website = detailsResult.Website;
+
+            if (photoReference != null)
+            {
+                // Execute the photo request
+                var photosResponse = await GooglePlaces.Photos.QueryAsync(new PlacesPhotosRequest
+                {
+                    Key = GoogleApiKey,
+                    PhotoReference = photoReference,
+                    MaxWidth = 400
+                });
+
+                if (photosResponse.PhotoBuffer != null)
+                {
+                    airportDetail.Photo = Convert.ToBase64String(photosResponse.PhotoBuffer);
+                    airportDetail.PhotoCredit = photoCredit;
+                }
+            }
+
+            return new JsonResult(airportDetail);
         }
     }
 }
